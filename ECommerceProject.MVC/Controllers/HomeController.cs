64e27f6a42ebe99d7efc967499f924a7cc78aef9
hassanahmed.dev25@gmail.com <hassanahmed.dev25@gmail.com>
2026-01-15 23:20:30 +0200

    feat: Add UI for home page to show all products

--- ECommerceProject.MVC/Controllers/HomeController.cs
+++ ECommerceProject.MVC/Controllers/HomeController.cs
@@ -1,22 +1,41 @@
-using System.Diagnostics;
+using ECommerceProject.Application.Services.Interfaces;
 using ECommerceProject.MVC.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace ECommerceProject.MVC.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IProductSurvice _productSurvice;
+        private readonly ICategoryService _categoryService;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IProductSurvice productSurvice, ICategoryService categoryService)
         {
             _logger = logger;
+            _productSurvice = productSurvice;
+            _categoryService = categoryService;
         }
 
-        public IActionResult Index()
+
+        // Show all products
+        public async Task<IActionResult> Index()
         {
-            return View();
+
+            var allProducts = await _productSurvice.GetAllProductsAsync();
+
+
+            var catgRes = await _categoryService.GetAllCategoriesAsync();
+
+            ViewBag.Categories = catgRes.result;
+
+            return View(allProducts.result);
         }
+
+
+
 
         public IActionResult Privacy()
         {
@@ -28,5 +47,11 @@ namespace ECommerceProject.MVC.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
+        
+        //public async Task<IActionResult> 
+
+
     }
 }
