decedd263e999acfc6d4adc58a9ba92e1a83bd91
hassanahmed.dev25@gmail.com <hassanahmed.dev25@gmail.com>
2026-01-16 16:01:43 +0200

    feat(cart): add cart page UI with selectable items

---
+++ ECommerceProject.Application/DTOs/Cart/CartItemDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerceProject.Application.DTOs.Cart
+{
+    public class CartItemDto
+    {
+        public string ProductName { get; set; }
+        public string ImageUrl { get; set; }
+        public double UnitPrice { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ ECommerceProject.MVC/Controllers/CartController.cs
@@ -0,0 +1,24 @@
+﻿using ECommerceProject.Application.DTOs.Cart;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceProject.MVC.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            var cartItemDto = new CartItemDto{ ProductName="s", Quantity=10, UnitPrice=20 };
+            var cartItemDto2 = new CartItemDto{ ProductName="s", Quantity=2, UnitPrice=2000 };
+            var cartItemDto3 = new CartItemDto{ ProductName="s", Quantity=34, UnitPrice=3424324 };
+
+            var cartItems = new List<CartItemDto>();
+
+            cartItems.Add(cartItemDto);
+            cartItems.Add(cartItemDto2);
+            cartItems.Add(cartItemDto3);
+
+            return View(cartItems);
+        }
+    }
+}
