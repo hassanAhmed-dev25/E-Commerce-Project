3479f7e138cf5b88157ac4b28f8429a737c46601
hassanahmed.dev25@gmail.com <hassanahmed.dev25@gmail.com>
2026-01-24 21:31:44 +0200

    feat(dashboard): create dashboard controller

---
+++ ECommerceProject.MVC/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceProject.MVC.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
