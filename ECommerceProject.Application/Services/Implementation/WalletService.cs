e92469ee8786798817b190be95621310d8841dd6
hassanahmed.dev25@gmail.com <hassanahmed.dev25@gmail.com>
2026-01-24 10:18:00 +0200

    faet(wallet): implement wallet service intarface

---
+++ ECommerceProject.Application/Services/Implementation/WalletService.cs
@@ -0,0 +1,43 @@
+﻿using ECommerceProject.Application.DTOs.Wallet;
+
+namespace ECommerceProject.Application.Services.Implementation
+{
+    public class WalletService : IWalletService
+    {
+
+        public Task<GetWalletDto> GetOrCreateWalletAsync(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+
+        public Task RequestWithdrawalAsync(int sellerId, decimal amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CompleteWithdrawalAsync(int withdrawalRequestId)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+
+
+        public Task ApproveWithdrawalAsync(int withdrawalRequestId)
+        {
+            throw new NotImplementedException();
+        }
+        
+
+        public Task RejectWithdrawalAsync(int withdrawalRequestId)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
