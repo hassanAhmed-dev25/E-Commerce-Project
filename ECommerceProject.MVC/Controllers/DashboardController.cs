2d52472850142f1a78df2a443337cf6c44f59ec4
hassanahmed.dev25@gmail.com <hassanahmed.dev25@gmail.com>
2026-01-24 21:54:06 +0200

    feat(dashboard): Implement seller UI dashboard

--- ECommerceProject.MVC/Controllers/DashboardController.cs
+++ ECommerceProject.MVC/Controllers/DashboardController.cs
@@ -1,12 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerceProject.MVC.Controllers
 {
+    [Authorize]
     public class DashboardController : Controller
     {
+
         public IActionResult Index()
+        {
+            if (User.IsInRole("Admin"))
+                return RedirectToAction("Admin");
+
+            if (User.IsInRole("Seller"))
+                return RedirectToAction("Seller");
+
+            return Forbid();
+        }
+
+
+        public IActionResult Seller()
         {
             return View();
         }
+
     }
 }
