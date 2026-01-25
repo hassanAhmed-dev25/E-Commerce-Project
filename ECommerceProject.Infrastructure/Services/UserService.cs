3e296d855762b4bed20cd8e750e24557a1f7f058
hassanahmed.dev25@gmail.com <hassanahmed.dev25@gmail.com>
2026-01-25 12:48:34 +0200

    feat(admin): implement user service interface

---
+++ ECommerceProject.Infrastructure/Services/UserService.cs
@@ -0,0 +1,12 @@
+﻿using ECommerceProject.Application.DTOs.Admin;
+
+namespace ECommerceProject.Infrastructure.Services
+{
+    public class UserService : IUserService
+    {
+        public Task<IEnumerable<GetUserDto>> GetAllWithRolesAsync(string email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
