ce5009f222b0df51fdc9adc1c48ac435e6b63b3c
hassanahmed.dev25@gmail.com <hassanahmed.dev25@gmail.com>
2026-01-28 13:13:44 +0200

    feat(account): add Dtos for external login

---
+++ ECommerceProject.Application/DTOs/Account/ExternalLoginDto.cs
@@ -0,0 +1,18 @@
+﻿namespace ECommerceProject.Application.DTOs.Account
+{
+    public class ExternalLoginDto
+    {
+        [Required]
+        public string Provider { get; set; } // Google, Microsoft, Facebook
+        [Required]
+        public string ProviderKey { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+    }
+
+}
---
+++ ECommerceProject.Application/DTOs/Account/ExternalLoginResultDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ECommerceProject.Application.DTOs.Account
+{
+    public class ExternalLoginResultDto
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+    }
+
+}
