8e8ab96a60f15cf0949cb260018784a11a4daeab
hassanahmed.dev25@gmail.com <hassanahmed.dev25@gmail.com>
2026-01-28 13:28:14 +0200

    feat(auth): complete external login service implementation

--- ECommerceProject.Infrastructure/Services/ExternalAuthService.cs
+++ ECommerceProject.Infrastructure/Services/ExternalAuthService.cs
@@ -4,9 +4,65 @@ namespace ECommerceProject.Infrastructure.Services
 {
     internal class ExternalAuthService : IExternalAuthService
     {
-        public Task<ExternalLoginResultDto> ExternalLoginAsync(ExternalLoginDto dto)
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        public ExternalAuthService(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
         {
-            throw new NotImplementedException();
+            _signInManager = signInManager;
+            _userManager = userManager;
         }
+
+        private async Task<string> GenerateUniqueUsernameAsync( string? firstName, string? lastName)
+        {
+            var baseUsername =
+                $"{firstName}.{lastName}".ToLower().Replace(" ", "");
+
+            while (true)
+            {
+                var username = $"{baseUsername}{Random.Shared.Next(1000, 9999)}";
+                var exists = await _userManager.FindByNameAsync(username);
+
+                if (exists == null)
+                    return username;
+            }
+        }
+
+
+
+        public async Task<ExternalLoginResultDto> ExternalLoginAsync(ExternalLoginDto dto)
+        {
+            
+            var user = await _userManager.FindByEmailAsync(dto.Email);
+
+            if (user == null)
+            {
+                user = new ApplicationUser
+                {
+                    UserName = await GenerateUniqueUsernameAsync(dto.FirstName, dto.LastName),
+                    Email = dto.Email,
+                    EmailConfirmed = true
+                };
+
+                await _userManager.CreateAsync(user);
+                await _userManager.AddLoginAsync(
+                    user,
+                    new UserLoginInfo(
+                        dto.Provider,
+                        dto.ProviderKey,
+                        dto.Provider)
+                    );
+            }
+
+            await _signInManager.SignInAsync(user, false);
+
+
+            return new ExternalLoginResultDto
+            {
+                IsSuccess = true,
+                Message = "Logged in successfully"
+            };
+
+        }
+
     }
 }
