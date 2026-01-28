c76252344b86bd8779f8edada2eb76244bfcb8d9
hassanahmed.dev25@gmail.com <hassanahmed.dev25@gmail.com>
2026-01-28 13:16:23 +0200

    feat(auth): add external login interface service

---
+++ ECommerceProject.Application/Services/Interfaces/IExternalAuthService.cs
@@ -0,0 +1,10 @@
+﻿using ECommerceProject.Application.DTOs.Account;
+
+namespace ECommerceProject.Application.Services.Interfaces
+{
+    public interface IExternalAuthService
+    {
+        Task<ExternalLoginResultDto> ExternalLoginAsync(ExternalLoginDto dto);
+
+    }
+}
