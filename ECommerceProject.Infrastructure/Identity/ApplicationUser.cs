5c0fc9637611fae669b0b807f940bc11ea24769a
hassanahmed.dev25@gmail.com <hassanahmed.dev25@gmail.com>
2026-01-31 14:14:34 +0200

    feat(admin): add IsBlocked property in ApplicationUser class

--- ECommerceProject.Infrastructure/Identity/ApplicationUser.cs
+++ ECommerceProject.Infrastructure/Identity/ApplicationUser.cs
@@ -6,5 +6,8 @@ namespace ECommerceProject.Infrastructure.Identity
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        public bool? IsBlocked { get; set; }
+
     }
 }
